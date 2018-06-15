d8a1a833604e622c8afb77d8dddaec58b201f9f6
Geoff Pleiss <gpleiss@gmail.com>
2018-06-15 07:42:47 -0700

    Move files, add setup

---
+++ setup.sh
@@ -0,0 +1,18 @@
+# Zsh
+ln -s zshrc ~/.zshrc
+touch ~/.zsh_path
+touch ~/.zsh_variables
+touch ~/.zsh_aliases
+
+# Git
+ln -s gitconfig ~/.gitconfig
+
+# Tmux
+ln -s tmux ~/.tmux
+ln -s tmux/tmux.conf ~/.tmux.conf
+
+# Jupyter
+ln -s jupyter ~/.jupyter
+
+# Matplotlib
+ln -s matplotlib ~/.matplotlib
