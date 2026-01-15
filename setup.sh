bd520fb92f73415e131b213b5f03914e21936bf1
Geoff Pleiss <824157+gpleiss@users.noreply.github.com>
2026-01-15 09:38:29 -0800

    Fix and update setup script

--- setup.sh
+++ setup.sh
@@ -3,33 +3,45 @@ if [ ! -d $HOME/.config ]; then
   mkdir .config
 fi
 
+# Get folder of this file
+DOTFILES_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
+
 # Zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -s zshrc ~/.zshrc
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
+[ ! -e ~/.zshrc ] && ln -s $DOTFILES_DIR/zshrc ~/.zshrc
 echo "export PATH=${PATH}" > ~/.zsh_path
 touch ~/.zsh_variables
 touch ~/.zsh_aliases
 
 # Git
-ln -s gitconfig ~/.gitconfig
+[ ! -e ~/.gitconfig ] && ln -s $DOTFILES_DIR/gitconfig ~/.gitconfig
+[ ! -e ~/.config/git ] && ln -s $DOTFILES_DIR/git ~/.config/git
+
+# Github
+[ ! -e ~/.config/gh ] && ln -s $DOTFILES_DIR/gh ~/.config/gh
 
 # Tmux
-ln -s tmux ~/.tmux
-ln -s tmux/tmux.conf ~/.tmux.conf
+[ ! -e ~/.tmux ] && ln -s $DOTFILES_DIR/tmux ~/.tmux
+[ ! -e ~/.tmux.conf ] && ln -s $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
 
 # Jupyter
-ln -s jupyter ~/.jupyter
+[ ! -e ~/.jupyter ] && ln -s $DOTFILES_DIR/jupyter ~/.jupyter
 
 # Matplotlib
-ln -s matplotlib ~/.config/matplotlib
+[ ! -e ~/.config/matplotlib ] && ln -s $DOTFILES_DIR/matplotlib ~/.config/matplotlib
 
 # Neovim
-ln -s nvim ~/.config/nvim
+[ ! -e ~/.config/nvim ] && ln -s $DOTFILES_DIR/nvim ~/.config/nvim
 
 # Pypi
-ln -s pypirc ~/.pypirc
+[ ! -e ~/.pypirc ] && ln -s $DOTFILES_DIR/pypirc ~/.pypirc
 
 # Fonts
 if [ -d "$HOME/Library/Fonts/" ]; then
   cp fonts/* $HOME/Library/Fonts
 fi
+
+# Wandb
+[ ! -e ~/.config/wandb ] && ln -s $DOTFILES_DIR/wandb ~/.config/wandb
